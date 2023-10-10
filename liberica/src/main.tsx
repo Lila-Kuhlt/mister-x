11b2c6c14603562ba575d2593de8b50100a38d0c
Aaron Geiger <aaron@geigr.dev>
2023-10-10 20:19:45 +0200

    Add BrwoserRouter

--- liberica/src/main.tsx
+++ liberica/src/main.tsx
@@ -2,13 +2,15 @@ import { CreateTeam } from "page/CreateeTeam";
 import { Home } from "page/Home";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/create-team" element={<CreateTeam />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-team" element={<CreateTeam />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
