aa1f246021b244fddc9a4191c38f76013578741a
Aaron Geiger <aaron@geigr.dev>
2023-10-10 20:00:30 +0200

    Add axios and ws

--- liberica/src/lib/websockts.ts
+++ liberica/src/lib/websockts.ts
@@ -1,1 +1,25 @@
-export class Websockets {}
+import { WebSocket } from "ws";
+import { ClientMessage, ServerMessage } from "lib/bindings";
+
+export type WSHandler = (msg: ServerMessage) => void;
+
+export class WebsocketApi {
+  connection: WebSocket;
+  handlers: WSHandler[];
+
+  constructor(endpoint: string) {
+    this.connection = new WebSocket(endpoint);
+
+    this.connection.onmessage;
+    this.handlers = [];
+  }
+
+  private register(handler: WSHandler): WebsocketApi {
+    this.handlers.push(handler);
+    return this;
+  }
+
+  private send(msg: ClientMessage) {
+    this.connection.send(JSON.stringify(msg));
+  }
+}
