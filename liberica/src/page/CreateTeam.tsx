8fb23b7bc8717ff42fdda8c16f6eb1d3e453c865
SimKien <simonkienle@web.de>
2023-10-10 21:57:01 +0200

    Add Create Team Component

--- liberica/src/page/CreateTeam.tsx
+++ liberica/src/page/CreateTeam.tsx
@@ -1,9 +1,26 @@
-import { Button } from "antd";
+import { ColorPicker, Input,  } from "antd";
+import "./CreateTeam.css"
+import { useState } from "react";
 
 export function CreateTeam() {
+  const [teamName, setTeamName] = useState('');
+  const [color, setColor] = useState('#000000');
+
   return (
-    <div>
-      <Button>Example</Button>
+    <div className="createTeamBox">
+      <div className="createTeamTitle">
+        <p>Create your Team</p>
+      </div>
+      <div className="adjustTeamBox">
+        <div className="teamName">
+          <label> Team Name:</label>
+          <Input placeholder="Teamname" onChange={(e) => setTeamName(e.target.value)}></Input>
+        </div>
+        <div className="pickTeamColor">
+          <label> Pick Team Color:</label>
+          <ColorPicker onChange={(_, hex) => setColor(hex)}></ColorPicker>
+        </div>
+      </div>
     </div>
   );
 }
