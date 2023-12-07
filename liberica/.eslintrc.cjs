29367e13608fd12153bb3871a11d1cad798676f0
konsumlamm <konsumlamm@gmail.com>
2023-12-07 18:49:13 +0100

    Update eslint config
    Only warn on unused vars

--- liberica/.eslintrc.cjs
+++ liberica/.eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
