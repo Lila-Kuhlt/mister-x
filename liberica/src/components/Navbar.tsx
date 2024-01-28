73a43bc75349bc79ae6b44db0abb9ce33471b3e9
konsumlamm <konsumlamm@gmail.com>
2024-01-28 15:45:48 +0100

    Partly fix navbar

--- liberica/src/components/Navbar.tsx
+++ liberica/src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import { PropsWithChildren } from "react";
 export function Navbar(props: PropsWithChildren) {
   return (
     <div
-      className="position-absolute bottom-0 w-max flex bg-white p-2 gap-3"
-      style={{ justifyContent: "space-between", alignItems: "center" }}
+      className="absolute bottom-0 w-max flex bg-white p-2 gap-3"
+      style={{ justifyContent: "space-between", alignItems: "center", zIndex: 1000 }}
     >
       {props.children}
     </div>
