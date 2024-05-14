1309162c6c54387b266574defe54b922e655c187
Aaron Geiger <aaron@geigr.dev>
2024-05-14 19:34:34 +0200

    Update eslint to v9

---
+++ liberica/eslint.config.js
@@ -0,0 +1,23 @@
+import globals from "globals";
+import eslintJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/* Currently some React related plugins are still not supported:
+ *
+ * - eslint-plugin-react
+ * - eslint-plugin-react-hooks
+ * - eslint-plugin-react-refresh
+ *
+ * For more information see the corresponding PRs:
+ * - https://github.com/jsx-eslint/eslint-plugin-react/pull/3743
+ */
+
+export default [
+    { ignores: ["src/lib/bindings.ts", "node_modules", "dist"] },
+    { languageOptions: { globals: globals.browser } },
+
+    eslintJs.configs.recommended,
+
+    ...tseslint.configs.strict, // strict is a superset of recommended
+    ...tseslint.configs.stylistic,
+];
