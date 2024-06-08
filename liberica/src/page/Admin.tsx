51b49fcd6b056224e500c29f5a1a6b4f2a6954f5
Aaron Geiger <aaron@geigr.dev>
2024-06-08 15:09:15 +0200

    Fix bg-white

--- liberica/src/page/Admin.tsx
+++ liberica/src/page/Admin.tsx
@@ -1,3 +1,6 @@
+import { useTranslation } from "react-i18next";
+
 export function Admin() {
-    return <div>Not yet implemented</div>;
+    const { t } = useTranslation();
+    return <div>{t("AdminPage")}</div>;
 }
