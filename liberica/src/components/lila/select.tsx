c01e698350f405719b6183ff01f09b267cc55a4f
Aaron Geiger <aaron@geigr.dev>
2024-06-20 23:33:30 +0200

    Setup select element

--- liberica/src/components/lila/select.tsx
+++ liberica/src/components/lila/select.tsx
@@ -1,8 +1,25 @@
-export type SelectProps = Omit<React.ComponentProps<"select">, "className">;
+import { classes } from "components/lila";
+
+export type SelectProps = Omit<
+    React.ComponentProps<"select">,
+    "className" | "size"
+> &
+    SelectPropsExt;
+export interface SelectPropsExt {
+    size?: keyof typeof INPUT_SIZES;
+    noLogo?: boolean;
+}
+
+export const BASE =
+    "text-on-muted rounded-xl bg-muted/20 outline-none ring-muted focus:ring-2";
+
+export const INPUT_SIZES = {
+    lg: "px-6 py-3 text-md",
+};
 
 export function Select(props: SelectProps) {
     return (
-        <select className="text-text rounded-xl bg-muted/20 px-6 py-3">
+        <select className={classes(BASE, INPUT_SIZES[props.size ?? "lg"])}>
             {props.children}
         </select>
     );
