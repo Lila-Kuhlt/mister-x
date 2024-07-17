db0e08382ae6e163c960856486321abfd00d157a
Aaron Geiger <aaron@geigr.dev>
2024-07-17 19:54:49 +0200

    Upate PRs in config doc & upgrade deps

--- liberica/eslint.config.js
+++ liberica/eslint.config.js
@@ -2,9 +2,14 @@ import globals from "globals";
 import eslintJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+/* import eslintPluginReact from "eslint-plugin-react";
+ * import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+ * import eslintPluginReactRefresh from "eslint-plugin-react-refresh";
+*/ import eslintPlugini18next from "eslint-plugin-i18next";
+
 // Prettier
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
-import eslintConfigPrettier from "eslint-config-prettier"
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /* Currently some React related plugins are still not supported:
  *
@@ -15,7 +20,8 @@ import eslintConfigPrettier from "eslint-config-prettier"
  *
  * For more information see the corresponding PRs:
  * - https://github.com/jsx-eslint/eslint-plugin-react/pull/3743
- * - https://github.com/edvardchen/eslint-plugin-i18next/pull/120
+ * - https://github.com/edvardchen/eslint-plugin-i18next/pull/126
+ * - https://github.com/facebook/react/pull/29770
  */
 
 /** @type {import("eslint").Config} */
@@ -26,12 +32,15 @@ export default [
             globals: globals.browser,
             parserOptions: {
                 project: true,
-                tsconfigRootDir: import.meta.dirname
-            }
-        }
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
 
     eslintJs.configs.recommended,
+
+    // eslintPluginReact.configs.recommended,
+    // eslintPlugini18next.configs.recommended,
 
     eslintPluginPrettier,
     eslintConfigPrettier, // disables some rules that cause conflicts
@@ -39,10 +48,9 @@ export default [
     ...tseslint.configs.strictTypeChecked, // strict is a superset of recommended
     ...tseslint.configs.stylistic,
 
-
     {
         rules: {
-            "@typescript-eslint/no-confusing-void-expression": "off"
-        }
-    }
+            "@typescript-eslint/no-confusing-void-expression": "off",
+        },
+    },
 ];
